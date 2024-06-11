047a26e247ec61e66935607e1e1ff9c73e30a014
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-06-11 11:47:34 +0200

    Started

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Routes, Route, NavLink } from 'react-router-dom'
 import DogForm from './DogForm'
 import DogsList from './DogsList'
